16d0653ef2a0c9b71d26563fc2945f16255de656
felix_makinda <felixmakinda@gmail.com>
2022-11-03 23:02:03 +0300

    Modified index.html and main.js

--- main.js
+++ main.js
@@ -60,5 +60,8 @@ window.addEventListener('load',() =>{
                 task_edit_el.innerText="Edit";
             }
         });
-    })
-})
+        task_delete_el.addEventListener('click', ()=>{
+            list_el.removeChild(task_el);
+        });
+    });
+});
